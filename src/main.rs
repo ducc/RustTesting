a48a0d015087acc62482065fb4d8dd621dea45c3
SpongyBacon <spongybacon@hotmail.co.uk>
2017-06-20 14:29:31 +0100

    encoding progress

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,33 @@ use hyper::client::{Body, Response};
 use byteorder::{BigEndian, LittleEndian, WriteBytesExt, ReadBytesExt};
 
 static URL: &'static str = "http://localhost:8080/tick";
-const TRACK_INFO_VERSIONED: i32 = 1;
+const TRACK_INFO_VERSIONED: u8 = 1;
+const TRACK_INFO_VERSION: u8 = 2;
 
 fn main() {
+    // debug
+    let enc_cursor = read_file("encoded.txt");
+    println!("main enc_cursor={:?}", enc_cursor.get_ref());
+    /*for c in enc_cursor.get_ref() {
+        print!("{}", *c as char);
+    }
+    println!();*/
+
     // decoding an encoded audio track
     let mut enc_cursor = read_file("encoded.txt");
     decode_track(&mut enc_cursor);
+
+    // encoding an audio track
+    encode_track(
+        "Alan Walker - Fade [NCS Release]", // title
+        "NCS", // author
+        264175, // duration
+        "177671751", // identifier
+        false, // stream
+        "https://soundcloud.com/nocopyrightsounds/alan-walker-fade-ncs-release", // uri
+        "soundcloud", // source
+        0 // position
+    );
 
     // sending an empty tick to remote node for statistics
     let mut buf = vec![];
@@ -42,30 +63,91 @@ fn move_cursor(cursor: &mut Cursor<Vec<u8>>, steps: u64) {
     cursor.set_position(pos + steps);
 }
 
+fn write_string(cursor: &mut Cursor<Vec<u8>>, content: &str) {
+    let _ = cursor.write_u8(content.len() as u8);
+    let _ = cursor.write(content.as_bytes());
+}
+
+fn write_string_and_incr(cursor: &mut Cursor<Vec<u8>>, content: &str) {
+    move_cursor(cursor, 1);
+    let _ = cursor.write_u8(content.len() as u8);
+    let _ = cursor.write(content.as_bytes());
+}
+
 fn read_string(cursor: &mut Cursor<Vec<u8>>) -> (String, usize) {
     let length = cursor.read_u8().unwrap() as usize;
     let mut b = vec![0; length];
     let _ = cursor.read_exact(&mut b);
-    (String::from_utf8(b).unwrap(), length)
+    let c = b.clone();
+    let s = String::from_utf8(b).unwrap();
+    println!("\nread_string b={:?}\nread_string String::from_utf8={}\n", &c, &s);
+    (s, length)
+}
+
+fn encode_track(title: &str, author: &str, duration: i64, identifier: &str, stream: bool, uri: &str,
+                source: &str, position: i64) {
+    let buf: Vec<u8> = vec![];
+    let mut cursor = Cursor::new(buf);
+    let _ = cursor.write_u8(TRACK_INFO_VERSION);
+
+    write_string_and_incr(&mut cursor, title);
+    write_string_and_incr(&mut cursor, author);
+
+    //0, 0, 0, 0, 0, 4, 7, -->239<--?
+    let _ = cursor.write_i64::<BigEndian>(duration);
+
+    move_cursor(&mut cursor, 3);
+    write_string(&mut cursor, identifier);
+
+    move_cursor(&mut cursor, 1);
+    if stream {
+        let _ = cursor.write(&[1]);
+    } else {
+        let _ = cursor.write(&[0]);
+    }
+
+    write_string_and_incr(&mut cursor, uri);
+    write_string_and_incr(&mut cursor, source);
+
+    let _ = cursor.write_i64::<BigEndian>(position);
+
+    println!("encode_track buf={:?}", cursor.get_ref());
+
+    let mut new_buf: Vec<u8> = vec![];
+    let _ = new_buf.write_i32::<BigEndian>(cursor.get_ref().len() as i32 | (TRACK_INFO_VERSIONED as i32) << 30);
+
+    let _ = new_buf.write_all(cursor.get_ref());
+
+    println!("encode_track new_buf={:?}", new_buf);
+
+    /*
+    prefix with size | flags << 30
+
+    public void commitMessage(int flags) throws IOException {
+      dataOutputStream.writeInt(messageByteOutput.size() | flags << 30);
+      messageByteOutput.writeTo(outputStream);
+    }
+    */
 }
 
 fn decode_track(cursor: &mut Cursor<Vec<u8>>) {
     let value = cursor.read_i32::<BigEndian>().unwrap();
     let m_flags = ((value as i64 & 0xC0000000) >> 30) as i32;
     let m_size = value & 0x3FFFFFFF;
-    println!("decode_track m_flags={} m_size={}", m_flags, m_size);
+    println!("decode_track value={} m_flags={} m_size={}", value, m_flags, m_size);
 
     /* position 1 and 2 might be part of java DataInputStream or BoundedInputStream, they are never
        used directly by lavaplayer */
     move_cursor(cursor, 2);
-    let mut buf = [0; 2];
-    let _ = cursor.read_exact(&mut buf);
-    let version = match m_flags & TRACK_INFO_VERSIONED {
+    let b = cursor.read_u8().unwrap();
+    let version = match m_flags & (TRACK_INFO_VERSIONED as i32) {
         0 => 1,
-        _ => buf[0] & 0xFF
+        _ => b & 0xFF
     };
-    println!("decode_track version={}", version);
+    println!("decode_track version={} b={:?}", version, b);
 
+    // move past empty byte
+    move_cursor(cursor, 1);
     let (title, length) = read_string(cursor);
     println!("decode_track title={} length={}", title, length);
 
@@ -100,6 +182,9 @@ fn decode_track(cursor: &mut Cursor<Vec<u8>>) {
     move_cursor(cursor, 1);
     let (source, length) = read_string(cursor);
     println!("decode_track length={} source={}", length, source);
+
+    let position = cursor.read_i64::<BigEndian>().unwrap();
+    println!("decode_track position={}", position);
 }
 
 fn post_tick(body: &[u8], size: usize) -> Cursor<Vec<u8>> {
